111bd57ee410abcd47e6ea8c46b0f0efb049eb38
Rachel Chan <rchan2023@gmail.com>
2025-11-06 12:18:58 -0800

    find random clustering centers

--- kmeans.py
+++ kmeans.py
@@ -7,7 +7,6 @@ from pathlib import Path
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import threading
 import os
 
 # read in coordinates from file to create dataset
@@ -20,6 +19,30 @@ def extract_coords(filename):
 
     return data
 
+# calculate euclidean distance to be used in creating distance matrix
+def euclidean_distance(point1, point2):
+    return math.sqrt((point1.iloc[0] - point2.iloc[0])**2 + (point1.iloc[1] - point2.iloc[1])**2)
+
+def create_distance_matrix(data):
+    n = len(data)
+    dist_matrix = np.zeros((n, n))   #N x N matrix of zeros
+   
+    for i in range(n):
+        for j in range(i, n):
+            dist_matrix[i, j] = dist_matrix[j, i] = euclidean_distance(data.iloc[i], data.iloc[j])
+
+    return dist_matrix
+
+def knn_clustering(data, k):
+    cluster_centers = set()
+
+    i = 0
+    while len(cluster_centers) < k:
+        index = random.randint(0, len(data))
+        cluster_centers.add(index) 
+
+    print(cluster_centers)
+
 def main():
     print('\nComputeDronePath')
 
@@ -29,6 +52,8 @@ def main():
     file_name,ext = os.path.splitext(file_name_w_ext)
     
     data = extract_coords(input_file)
+
+    # knn_clustering(data, 4)
 
 if __name__ == '__main__':
   main()
